e8c51edc3c666341eae8efad09e93e30445a7777
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-11 13:34:03 +0900

    Test: 장소명 검색을 통한 배드민턴장 목록 조회 mapper 테스트

--- badminton/src/test/java/com/project/badminton/mapper/CourtMapperTests.java
+++ badminton/src/test/java/com/project/badminton/mapper/CourtMapperTests.java
@@ -15,11 +15,23 @@ public class CourtMapperTests {
 	@Autowired
 	private CourtMapper courtMapper;
 	
-	//CourtMapper.java의 selectCourtList 메서드 테스트 코드
+	//CourtMapper.java의 selectCourtListByLocation 메서드 테스트 코드
 	@Test
-	public void testOfSelectCourtList() {
+	public void testOfSelectCourtListByLocation() {
 		CourtReqDTO courtReqDTO = new CourtReqDTO("인천광역시", "남동구", "구월동");
 		List<CourtDTO> courts = courtMapper.selectCourtListByLocation(courtReqDTO);
+		
+		if (courts.size()!=0) {
+			for (CourtDTO court: courts) {
+				System.out.println(court.toString());
+			}
+		}
+	}
+	
+	//CourtMapper.java의 selectCourtListByName 메서드 테스트 코드
+	@Test
+	void testOfSelectCourtListByName() {
+		List<CourtDTO> courts = courtMapper.selectCourtListByName("향촌");
 		
 		if (courts.size()!=0) {
 			for (CourtDTO court: courts) {
