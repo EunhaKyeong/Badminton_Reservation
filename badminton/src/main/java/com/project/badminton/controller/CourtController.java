2f010e083c88c544132448dd47d7f6266f159c82
EunhaKyeong <kyeongeh424@gmail.com>
2021-11-16 22:50:21 +0900

    Feat: 배드민턴 상세 정보를 전달하는 API 컨트롤러 작성

--- badminton/src/main/java/com/project/badminton/controller/CourtController.java
+++ badminton/src/main/java/com/project/badminton/controller/CourtController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ import com.project.badminton.domain.CourtDTO;
 import com.project.badminton.domain.CourtReqDTO;
 import com.project.badminton.service.CourtService;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/courts")
 public class CourtController {
@@ -26,7 +27,10 @@ public class CourtController {
 	
 	//배드민턴장 목록 조회하기
 	@GetMapping() 
-	ResponseEntity<List<CourtDTO>> getCourtListByLocation(@RequestParam(value="metropolitanCity", required=false) String metropolitanCity, @RequestParam(value="sigungu", required=false) String sigungu, @RequestParam(value="eupmyeonri", required=false) String eupmyeonri, @RequestParam(value="name", required=false) String name) throws UnsupportedEncodingException {
+	ResponseEntity<List<CourtDTO>> getCourtListByLocation(@RequestParam(value="metropolitanCity", required=false) String metropolitanCity, 
+														@RequestParam(value="sigungu", required=false) String sigungu, 
+														@RequestParam(value="eupmyeonri", required=false) String eupmyeonri, 
+														@RequestParam(value="name", required=false) String name) throws UnsupportedEncodingException {
 		List<CourtDTO> courts = new ArrayList<CourtDTO>();
 		
 		if (metropolitanCity!=null) {	//장소(광역시도/시군구/읍면동리)를 검색해 배드민턴장 목록 검색하기
@@ -42,6 +46,18 @@ public class CourtController {
 		} else {
 			//검색 결과 해당 위치에 존재하는 배드민턴 장소가 있다면 200 Status Code와 배드민턴장 목록 데이터 전달.
 			return ResponseEntity.ok(courts);
+		}
+	}
+	
+	//배드민턴장 id를 가지고 배드민턴장 상세정보 조회하기
+	@GetMapping("/{id}")
+	ResponseEntity<CourtDTO> getCourtById(@PathVariable Long id) {
+		CourtDTO court = courtService.getCourtById(id);
+		
+		if (court==null) {
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.ok(court);
 		}
 	}
 }
